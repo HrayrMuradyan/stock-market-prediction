fa1c004fd2a0732151d336bda8aac1b8c3c59379
Hrayr Muradyan <hrayrmuradyan20@gmail.com>
2025-07-25 17:29:32 -0700

    Added pyproject.toml, added config file there. Moved pytest.ini to pyproject.toml

--- src/utils/path.py
+++ src/utils/path.py
@@ -111,3 +111,26 @@ def verify_existing_path(path):
     # If the path is not a directory, raise an error
     if not path.is_dir():
         raise ValueError(f"Path '{path}' is not a directory.")
+    
+
+def get_project_root_path(levels_up):
+    """
+    Return the path to the project root by traversing up a given number of directory levels
+    from the location of the current file.
+
+    Parameters:
+        levels_up (int): The number of parent directories to go up from the current file.
+
+    Returns:
+        Path: The resolved absolute path to the computed root directory.
+
+    """
+    # Get the absolute path of the file
+    path = Path(__file__).resolve()
+
+    # If the number of levels is larger than the number of parents raise an error
+    if levels_up >= len(path.parents):
+        raise ValueError(f"levels_up={levels_up} is too high for path {path}")
+    
+    # Get the project root
+    return path.parents[levels_up]
